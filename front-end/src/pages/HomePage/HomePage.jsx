3a8f1f68e7248bc829864f4c7ad536c70f734c6b
Musa <Musaxlilov29@gmail.com>
2024-12-23 22:29:35 +0400

    Home Page add

--- front-end/src/pages/HomePage/HomePage.jsx
+++ front-end/src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,10 @@
-// const HomePage = () => {
-//     return (
-//         <div>
-            
-//         </div>
-//     )
-// }
-
-// export default HomePage;
 
 import dropdown_icon_down from "../../assets/dropdown_icon.svg";
 import dropdown_icon_up from "../../assets/dropdown_icon_up.svg";
 import { useEffect, useState } from "react";
+import HomePageCoins from "../HomePageCoins/HomePageCoins";
+import './HomePage.css';
+import HomePageAdvanced from "../HomePageAdvanced/HomePageAdvanced";
 
 
 const HomePage = () => {
@@ -25,12 +19,12 @@ const HomePage = () => {
         <div className="homepage-wrapper">
             HomePage
             <div className="homepage-input-wrapper">
-                <label className="search-label" htmlFor="home-search">Input field</label>
+                <label className="input-label" htmlFor="home-search">Input field</label>
                 <input className="search-input" name="home-search" />
                 <button className="search-button" >Search</button>
             </div>
-            <a className="advanced" onClick={() => setToggleAdvanced(!toggleAdvanced)} href="#">Advanced Filter {toggleAdvanced ? <img src={dropdown_icon_up} /> : <img src={dropdown_icon_down} />}</a>
-            {/* {toggleAdvanced ? <HomePageCoins /> : <HomePageCoins />} */}
+            <a className="advanced" onClick={() => setToggleAdvanced(!toggleAdvanced)} href="#"><span>Advanced Filter</span> {toggleAdvanced ? <img src={dropdown_icon_up} /> : <img src={dropdown_icon_down} />}</a>
+            {toggleAdvanced ? <HomePageAdvanced /> : <HomePageCoins />}
         </div>
     )
 }
---
+++ front-end/src/pages/HomePageAdvanced/HomePageAdvanced.jsx
@@ -0,0 +1,10 @@
+
+const HomePageAdvanced = () => {
+    return(
+        <div className="homepage-advanced-wrapper">
+            <label>Issuing country</label>
+        </div>
+    )
+}
+
+export default HomePageAdvanced;
